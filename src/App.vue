90d486fdecda9499589f8cd4f442a797f9d37db3
amordz72 <dzamor72@gmail.com>
2022-05-26 09:19:08 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,12 @@ import SideBar from "./components/SideBar.vue";
     <SideBar></SideBar>
     <div class="flex-auto bg-slate-300 h-screen">
       <div class="mt-4 px-4">
-
-
-        <div class="d-flex">
-          <h4 class="text-2xl font-bold mb-2">Dashboard</h4>
-          <!-- <p class="text-1xl font-semibold">10-05-2022 / <span>5:14</span></p> -->
-        </div>
+        <!--   <div class="d-flex">
+          
+            <h4 class="text-2xl font-bold mb-2">Dashboard
+              
+            </h4> <p class="text-1xl font-semibold">10-05-2022 / <span>5:14</span></p> 
+        </div>-->
 
         <div class="content">
           <router-view />
