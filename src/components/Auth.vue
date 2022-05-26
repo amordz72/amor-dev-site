3876867c628295e98615e3aa32f0e35afdad2f29
amordz72 <dzamor72@gmail.com>
2022-05-26 14:12:23 +0100

    befor model-auth

---
+++ src/components/Auth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="">
+ <!-- Modal toggle -->
+ 
+ 
+
+
+
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    tgl() {
+      document.getElementById("exampleModal").classList.toggle("hidden");
+    },
+  },
+};
+</script>
+
+<style scoped>
+ #i-tgl:checked + #exampleModal {
+  display: flow ;
+} /**/
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="">Home</div>
+  <div class=" ">
+    Home
+ <ModelAuth></ModelAuth>
+  </div>
 </template>
  
 <script>
+import  ModelAuth  from "../components/Auth.vue";
+
 export default {
   name: "HomeView",
-  components: {},
+  components: { ModelAuth },
 };
 </script>
 <style scoped>
